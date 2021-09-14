b17bc322ad6e9b7699e35bbb69b81e3b381d3a1e
tracer-ics2021 <tracer@njuics.org>
2021-09-14 22:31:35 +0800

    >  compile 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 371d216969af45de84ccf53eab2519a6   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  22:31:35 up  2:28,  5 users,  load average: 0.02, 0.09, 0.14

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -3,7 +3,7 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "sdb.h"
-
+#include <utils.h>
 static int is_batch_mode = false;
 
 void init_regex();
@@ -34,6 +34,7 @@ static int cmd_c(char *args) {
 
 
 static int cmd_q(char *args) {
+  nemu_state.state = NEMU_QUIT;
   return -1;
 }
 

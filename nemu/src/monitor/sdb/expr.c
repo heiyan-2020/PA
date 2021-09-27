04441af69d54a20d1b412ba2f9d24de5715023d6
tracer-ics2021 <tracer@njuics.org>
2021-09-27 20:10:44 +0800

    >  compile 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 52ab2ddb1c334e89b1693b1fdc2f7ddb   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  20:10:44 up  9:24,  5 users,  load average: 0.37, 0.24, 0.38

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -144,6 +144,7 @@ word_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
+	printf("make %d tokens\n", nr_token);
 	return (word_t) eval(0, nr_token- 1);
 }
   /* TODO: Insert codes to evaluate the expression. */

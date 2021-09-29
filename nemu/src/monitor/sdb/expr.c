361d63c45746afaa0b5f3064bd7c8e25598070e0
tracer-ics2021 <tracer@njuics.org>
2021-09-29 17:04:00 +0800

    >  compile 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 92752bd3dc21483db53ffec297583d62   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  17:04:00 up 27 min,  3 users,  load average: 0.81, 0.25, 0.11

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -1,18 +1,18 @@
 #include <isa.h>
 #include <string.h>
+#include <memory/paddr.h>
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
  */
 #include <regex.h>
-
-int precedence[32] = {0, 6, 10, 6, 5, 5, 3, 3, 0};
+int precedence[32] = {0, 4, 7, 4, 3, 3, 1, 1, 0, 0, 0, 7, 11, 2};
 
 enum {
   TK_NOTYPE, TK_PLUS, TK_EQ, TK_SUB, TK_DIV, TK_MUL, TK_LEFT, TK_RIGHT, TK_NUM, TK_HEXNUM, TK_REG, TK_NEQ, TK_AND, TK_DEREF
 };
 int find_op(int begin, int end);
 bool check_parentheses(int begin, int end);
-uint32_t eval(int begin, int end) ;
+uint32_t eval(int begin, int end, bool* success3) ;
 static struct rule {
   const char *regex;
   int token_type;
@@ -115,39 +115,53 @@ static bool make_token(char *e) {
 								case TK_PLUS:
 								case TK_SUB:
 								case TK_LEFT:
+								case TK_EQ:
+								case TK_NEQ:
+								case TK_AND:
 								case TK_RIGHT: {
 									struct token tmp_token;
 									tmp_token.type = rules[i].token_type;
 									tokens[nr_token++] = tmp_token;
 									break;
 							  }
+								case TK_REG:
 								case TK_NUM: {
+									assert(substr_len < 32);
 									if (substr_len < 32) {
 										struct token single_token;	
 										single_token.type = TK_NUM;
 										strncpy(single_token.str, substr_start, substr_len);
 										single_token.str[substr_len] = '\0';
 										tokens[nr_token++] = single_token;
-									}	else {
-													assert(0);
-										int offset = 0;
-										while (substr_len - offset >= 32) {
-											struct token single_token;
-											single_token.type = TK_NUM;
-											strncpy(single_token.str, substr_start + offset, 31);
-											single_token.str[31] = '\0';
-											offset += 31;
-											tokens[nr_token++] = single_token;
-										}
-										struct token single_token;
-										single_token.type = TK_NUM;
-										strncpy(single_token.str, substr_start + offset, substr_len - offset);
-										single_token.str[substr_len - offset] = '\0';
-										tokens[nr_token++] = single_token;
-									}					
+									}
 									break;
+								//	else {
+								//					assert(0);
+								//		int offset = 0;
+								//		while (substr_len - offset >= 32) {
+								//			struct token single_token;
+								//			single_token.type = TK_NUM;
+								//			strncpy(single_token.str, substr_start + offset, 31);
+								//			single_token.str[31] = '\0';
+								//			offset += 31;
+								//			tokens[nr_token++] = single_token;
+								//		}
+								//		struct token single_token;
+								//		single_token.type = TK_NUM;
+								//		strncpy(single_token.str, substr_start + offset, substr_len - offset);
+								//		single_token.str[substr_len - offset] = '\0';
+								//		tokens[nr_token++] = single_token;
+								//	}					
 								}
-          default: break;
+								case TK_HEXNUM: {
+									Token single_token;
+									single_token.type = TK_HEXNUM;
+									strncpy(single_token.str, substr_start + 2, substr_len);
+									single_token.str[substr_len] = '\0';
+									tokens[nr_token++] = single_token;
+									break;									
+								}
+         				default: break;
         }
 
         break;
@@ -175,37 +189,65 @@ word_t expr(char *e, bool *success) {
 		tokens[i].type = TK_DEREF;
 		}
 	}
-
-	return (word_t) eval(0, nr_token- 1);
+	
+	int result = eval(0, nr_token - 1, success);
+	return result;
 }
   /* TODO: Insert codes to evaluate the expression. */
-uint32_t eval(int begin, int end) {
+uint32_t eval(int begin, int end, bool* success) {
 	if (begin > end) {
-		assert(0);
+		printf("A syntax error in expression!\n");
+		assert(0);	
 	} else if(begin == end) {
-		uint32_t rtnValue;
-		assert(tokens[begin].type == TK_NUM);
-		sscanf(tokens[begin].str, "%u", &rtnValue);
-		return rtnValue;
-	} else if(check_parentheses(begin, end) == true){
+			uint32_t result = 0;
+			switch (tokens[begin].type){
+							case TK_NUM: {
+														sscanf(tokens[begin].str, "%u", &result);
+														break;				
+													 }
+							case TK_HEXNUM: {
+															sscanf(tokens[begin].str, "%x", &result);
+															break;
+															}
+							case TK_REG: {
+													 result = isa_reg_str2val(tokens[begin].str, success);
+													 break;
+													 }
+							default :break;
+		}	
+			return result;
+} else if(check_parentheses(begin, end) == true){
 		if (begin + 1 > end - 1) {
 		}
 	
-		return eval(begin + 1, end - 1);
+		return eval(begin + 1, end - 1, success);
 	} else {
 		int op = find_op(begin, end);
-		int lhs = eval(begin, op - 1);
-		int rhs = eval(op + 1, end);
-		switch (tokens[op].type) {
-			case TK_PLUS : return lhs + rhs;
-			case TK_SUB : return lhs - rhs;
-			case TK_MUL : return lhs * rhs;
-			case TK_DIV : return lhs / rhs;
-			case TK_EQ : return lhs == rhs;
-			case TK_NEQ: return lhs != rhs;
-			case TK_AND: return lhs && rhs;
-			default : assert(0);
-		}		
+		if (tokens[op].type != TK_DEREF) {
+			int lhs = eval(begin, op - 1, success);
+			int rhs = eval(op + 1, end, success);
+			switch (tokens[op].type) {
+				case TK_PLUS : return lhs + rhs;
+				case TK_SUB : return lhs - rhs;
+				case TK_MUL : return lhs * rhs;
+				case TK_DIV : return lhs / rhs;
+				case TK_EQ : return lhs == rhs;
+				case TK_NEQ: return lhs != rhs;
+				case TK_AND: return lhs && rhs;
+				default : assert(0);
+			}		
+		} else {
+			assert(tokens[op].type == TK_DEREF);
+			uint32_t result = 0;
+			paddr_t address;
+			sscanf(tokens[op].str, "%x", &address);
+			for (int i = 3; i >= 0; i --) {
+				uint8_t bits = *(guest_to_host(address + (3 - i)));
+				uint32_t tmp = (uint32_t) bits;
+				result |= (tmp << (8 * i));
+			}
+			return result;
+		}
 	}
 	return 0;
 }

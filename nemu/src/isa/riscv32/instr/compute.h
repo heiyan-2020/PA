1da04a1c8187566857d37cb6874c456db1c442a4
tracer-ics2021 <tracer@njuics.org>
2021-10-10 20:19:17 +0800

    >  compile 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 546c20a6204342849fea3a15c6049ec2   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  20:19:17 up  1:24,  4 users,  load average: 0.21, 0.25, 0.34

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -1,3 +1,10 @@
 def_EHelper(lui) {
   rtl_li(s, ddest, id_src1->imm);
 }
+
+
+def_EHelper(addi) {
+	sword_t tmp = ((id_src2->imm << 30) >> 31) & 0xFFFFF000;
+	const	sword_t sign_extension = id_src2->imm | tmp;
+	rtl_addi(s, ddest, dsrc1, sign_extension);
+}

48ee48b920bb7d68ec1a6fda8196ebdedc214d62
tracer-ics2021 <tracer@njuics.org>
2021-10-23 09:45:10 +0800

    >  compile 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: bf59edf10fce43339d33e8ef45c5b5bb   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  09:45:10 up 25 min,  4 users,  load average: 0.16, 0.19, 0.17

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -15,5 +15,12 @@ void isa_reg_display() {
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
-  return 0;
+		for (int i = 0; i < 32; i ++) {
+			if (strcmp(regs[i], s)) {
+				return cpu.gpr[i]._32;
+			}
+		}
+		*success = false;
+		return 0;
 }
+

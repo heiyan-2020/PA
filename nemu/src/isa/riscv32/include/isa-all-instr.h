f67df5129a75c4d2fe1d8a4fc489ae8c57cc96a9
tracer-ics2021 <tracer@njuics.org>
2021-10-24 13:32:41 +0800

    >  compile 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: b05e89b0fda7423c8fecccc4e53db382   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  13:32:41 up 14:14,  4 users,  load average: 0.35, 0.26, 0.16

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -1,6 +1,6 @@
 #include <cpu/decode.h>
 #include "../local-include/rtl.h"
 
-#define INSTR_LIST(f) f(lui) f(lw) f(sw) f(inv) f(nemu_trap) f(addi) f(auipc) f(jal) f(jalr) f(add) f(sub) f(sltiu) f(beq) f(bne) f(sltu) f(myxor) f(myor) f(sb) f(srai) f(lbu) f(andi) f(sll) f(myand) f(xori) f(bge) f(div) f(slli) f(bltu) f(lh) f(lhu) f(srli) f(sh) f(rem) f(mul) f(mulh) f(blt)
+#define INSTR_LIST(f) f(lui) f(lw) f(sw) f(inv) f(nemu_trap) f(addi) f(auipc) f(jal) f(jalr) f(add) f(sub) f(sltiu) f(beq) f(bne) f(sltu) f(myxor) f(myor) f(sb) f(srai) f(lbu) f(andi) f(sll) f(myand) f(xori) f(bge) f(div) f(slli) f(bltu) f(lh) f(lhu) f(srli) f(sh) f(rem) f(mul) f(mulh) f(blt) f(slt)
 
 def_all_EXEC_ID();

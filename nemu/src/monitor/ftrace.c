c99dae866e70b6b601a2c3cc32419e971e337970
tracer-ics2021 <tracer@njuics.org>
2021-10-25 21:12:54 +0800

    >  compile 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 28f0d31fcb4742a1920768ba5afb89f2   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  21:12:54 up  5:08,  3 users,  load average: 0.17, 0.23, 0.32

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -35,10 +35,9 @@ void init_ftrace() {
 	//load each symbol
 	symbol_pool = (Elf32_Sym*) malloc(symbol_table.sh_size);
 	fseek(ftrace_fp, symbol_table.sh_offset, SEEK_SET);
-	printf("DEBUUG INFO:offset is %d\n", symbol_table.sh_offset);
 	item_count = fread(symbol_pool, 1, symbol_table.sh_size, ftrace_fp);
-	printf("DEBUG INFO:%d\n", item_count);
 	assert(item_count == symbol_table.sh_size);
+	printf("DEBUG INFO:the number of items is %d\n", symbol_table.sh_size / symbol_table.sh_entsize);
 	for(int i = 0; i < symbol_table.sh_size / symbol_table.sh_entsize; i += 1) {
 		printf("%s\t0x%x\n", str_pool + (symbol_pool + i)->st_name, (symbol_pool + i)->st_value);
 	}

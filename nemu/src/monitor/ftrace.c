45f5a020b9cc1f4c51b7dbbcd70e7c062b0ad482
tracer-ics2021 <tracer@njuics.org>
2021-10-25 20:47:06 +0800

    >  compile 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 28f0d31fcb4742a1920768ba5afb89f2   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  20:47:06 up  4:42,  3 users,  load average: 0.94, 0.86, 0.85

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -1,5 +1,6 @@
 #include <isa.h>
 #include <stdio.h>
+#include <string.h>
 extern char* elf_file; 
 Elf32_Ehdr elf_header;
 Elf32_Shdr string_table;
@@ -15,7 +16,20 @@ void init_ftrace() {
 	fseek(ftrace_fp, elf_header.e_shstrndx * elf_header.e_shentsize, SEEK_CUR);
 	item_count = fread(&string_table, 40, 1, ftrace_fp);
 	assert(item_count == 1);
-	printf("%u\n", elf_header.e_shentsize);
-	printf("%u\n", string_table.sh_name);
+	char* str_pool = (char*) malloc(string_table.sh_size);
+	fseek(ftrace_fp, string_table.sh_offset, SEEK_SET);
+	item_count = fread(str_pool, 1, string_table.sh_size, ftrace_fp);
+	assert(item_count == string_table.sh_size);
+	//find Symbol table
+	fseek(ftrace_fp, elf_header.e_shoff, SEEK_SET);
+	do {
+			item_count = fread(&symbol_table, 40, 1, ftrace_fp);
+			assert(item_count == 1);
+			char* name = str_pool + symbol_table.sh_name;
+			if (strcmp(name, ".stmtab") == 0) {
+				break;
+			}	
+	} while (1);
+	printf("DEBUG INFO:Find Successfully!\n");
 	fclose(ftrace_fp);
 }

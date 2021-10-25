afd543387d3f653891c79fb4bdbe8d0c07689dc0
tracer-ics2021 <tracer@njuics.org>
2021-10-25 23:50:58 +0800

    >  compile 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: d0c4b5de2fba4d84bb03b36cea31aef9   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  23:50:58 up  1:35,  4 users,  load average: 0.19, 0.17, 0.24

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -73,7 +73,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
 			case 'e':	elf_file = optarg; printf("DEBUG INFO:file is %s\n", elf_file); break; 
-			case 'f': ftrace_file = optarg;break;
+			case 'f': printf ("DEBUG INFO:ftrace\n"); ftrace_file = optarg;break;
       case 1: img_file = optarg; return optind - 1;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);

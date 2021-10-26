8ca808c762971d584ab87d1bfb6648a764136adb
tracer-ics2021 <tracer@njuics.org>
2021-10-26 10:58:11 +0800

    >  compile 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 05df047e456649ea8130cf1206762f55   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  10:58:12 up 43 min,  3 users,  load average: 0.55, 0.52, 0.53

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -83,10 +83,11 @@ bool func_call(uint32_t addr, uint32_t site) {
 	while (count < symbol_table.sh_size / symbol_table.sh_entsize) {
 		if (itr->st_value == addr) {
 			//function call.
-			printf("DEBUG INFO: itr->st_name is %u\n", itr->st_name);
+			printf("DEBUG PAIR: itr->st_name is %u\n", itr->st_name);
 			sprintf(buffer, "[0x%x]\tcall", site);
 			strcat(log, buffer);
 			sprintf(buffer, "[%s@0x%x]\n", str_pool + (itr->st_name), addr);
+			printf("DEBUG PAIR: after calling normal!\n");
 			strcat(log, buffer);
 			print_ftrace(log);
 			return true;

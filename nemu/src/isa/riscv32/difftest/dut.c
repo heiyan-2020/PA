f7f85c5b7df763fd991c5b8e52135a346597fa09
tracer-ics2021 <tracer@njuics.org>
2021-10-26 15:13:37 +0800

    >  compile 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 05df047e456649ea8130cf1206762f55   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  15:13:37 up  4:59,  3 users,  load average: 0.86, 1.14, 1.04

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -3,16 +3,15 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-//	for (int i = 0; i < 32; i++) {
-//		if (ref_r->gpr[i]._32 != cpu.gpr[i]._32) {
-//			return false;
-//		}
-//	}
-//	if (pc != cpu.pc) {
-//		return false;
-//	}	
-//	return true;
-//  return false;
+	for (int i = 0; i < 32; i++) {
+		printf("ref's reg is 0x%x, while dut's is 0x%x\n", ref_r->gpr[i]._32, cpu.gpr[i]._32);
+		if (ref_r->gpr[i]._32 != cpu.gpr[i]._32) {
+			return false;
+		}
+	}
+	if (pc != cpu.pc) {
+		return false;
+	}	
   return true;
 }
 

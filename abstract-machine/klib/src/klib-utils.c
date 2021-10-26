e10a1ee025de16343421dc63fa105dc6519e8267
tracer-ics2021 <tracer@njuics.org>
2021-10-26 21:35:34 +0800

    >  compile 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 7f9e6718e56541239d8e26e6b4267f77   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  21:35:34 up  5:21,  4 users,  load average: 0.25, 0.21, 0.28

---
+++ abstract-machine/klib/src/klib-utils.c
@@ -0,0 +1,51 @@
+//convert int/uint number to string accroding to base.
+#include <stdarg.h>
+#include <klib.h>
+char * convert(uint32_t num, int base) {
+	static char Table[] = "0123456789ABCDEF";
+	static char buffer[50];
+	char* ptr;
+
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do {
+		*--ptr = Table[num % base];
+		num /= base;
+	}while (num != 0);
+
+	return ptr;
+}
+//abstract the repetitive code of print family func.
+char* print_template(char* out, const char* fmt, va_list ap) {
+		int d;
+		char *s;
+		char *ptr = out;
+		while (*fmt) {
+			switch (*fmt++) {
+				case '%': {
+					switch (*fmt++) {
+						case '%': *out++ = '%';break;
+						case 'd': {
+							d = va_arg(ap, int);
+							char* tmp_str = convert(d, 10);
+							memcpy(out, tmp_str, strlen(tmp_str));
+							out += strlen(tmp_str);
+							break;
+						}
+						case 's': {
+							s = va_arg(ap, char*);
+							memcpy(out, s, strlen(s));
+							out += strlen(s);
+							break;					
+						}
+						default: return NULL;
+					}	
+				}
+									break;
+				default : *out++ = *(fmt - 1);break;
+			}
+		}
+		*out = '\0';
+		return ptr;
+}
--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -3,21 +3,7 @@
 #include <klib-macros.h>
 #include <stdarg.h>
 #include <stdint.h>
-char * convert(uint32_t num, int base) {
-	static char Table[] = "0123456789ABCDEF";
-	static char buffer[50];
-	char* ptr;
-
-	ptr = &buffer[49];
-	*ptr = '\0';
-
-	do {
-		*--ptr = Table[num % base];
-		num /= base;
-	}while (num != 0);
-
-	return ptr;
-}
+#include <klib-utils.h>
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
@@ -30,40 +16,11 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 
 int sprintf(char *out, const char *fmt, ...) {
 		va_list ap;
-		int d;
-		char *s;
-		char *ptr = out;
 		va_start(ap, fmt);
-		while (*fmt) {
-			switch (*fmt++) {
-				case '%': {
-					switch (*fmt++) {
-						case '%': *out++ = '%';break;
-						case 'd': {
-							d = va_arg(ap, int);
-							char* tmp_str = convert(d, 10);
-							memcpy(out, tmp_str, strlen(tmp_str));
-							out += strlen(tmp_str);
-							break;
-						}
-						case 's': {
-							s = va_arg(ap, char*);
-							memcpy(out, s, strlen(s));
-							out += strlen(s);
-							break;					
-						}
-						default: return -1;
-					}	
-				}
-									break;
-				default : *out++ = *(fmt - 1);break;
-			}
-		}
+		print_template(out, fmt, ap);
+		int len = strlen(out);
 		va_end(ap);
-		int num = out - ptr - 1;
-		*out = '\0';
-		out = ptr;
-		return num;
+		return len;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {

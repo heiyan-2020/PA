178c8b079a09c592f8a54235d8e4e391f3f6f63a
tracer-ics2021 <tracer@njuics.org>
2021-10-29 22:01:33 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 4fd94e09ebeb489fbd1841ea9989e173   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  22:01:33 up  2:11,  6 users,  load average: 0.21, 0.25, 0.26

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,11 +5,7 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-		uint64_t result = 0;
-		result = inl(RTC_ADDR);
-		result <<= 32;
-		result += inl(RTC_ADDR + 4);
-		printf("%ld\n", result);
+				printf("first is %u\n", inl(RTC_ADDR));
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {

a0cfd53adc38ea9a7a5c45d10ae8d49b81e3d532
tracer-ics2021 <tracer@njuics.org>
2021-10-30 11:24:08 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 3e133e6361f3477d8aa5e8dee77a4feb   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  11:24:08 up  2:30,  5 users,  load average: 0.08, 0.08, 0.13

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+		uint32_t state = inl(KBD_ADDR);
+		kbd->keydown = state >> 31;
+		kbd->keycode = (int)((state << 1) >> 1);
 }

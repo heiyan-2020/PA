3a83a6204f0872404d319e6505f00c8d0cb79a67
tracer-ics2021 <tracer@njuics.org>
2021-10-30 10:39:18 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 3e133e6361f3477d8aa5e8dee77a4feb   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  10:39:18 up  1:45,  4 users,  load average: 0.10, 0.08, 0.09

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -39,8 +39,8 @@ void init_map() {
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
 #ifdef CONFIG_DTRACE
+	log_write("read [%s] at 0x%x\n", map->name, addr);
 #endif
-	printf("read %s at 0x%x\n", map->name, addr);
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
@@ -50,6 +50,9 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 }
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
+#ifdef CONFIG_DTRACE
+	log_write("write [%s] to 0x%x\n", map->name, addr);
+#endif
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;

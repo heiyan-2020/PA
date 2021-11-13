b67ca1068fb1933e1ce6e2fb55886a9e3afae0f6
tracer-ics2021 <tracer@njuics.org>
2021-11-13 22:10:55 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 6800c070e6414211929c4a518a7d8e8c   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  22:10:55 up  2:44,  3 users,  load average: 0.05, 0.06, 0.09

--- nemu/include/cpu/ifetch.h
+++ nemu/include/cpu/ifetch.h
@@ -3,6 +3,7 @@
 #include <memory/vaddr.h>
 
 static inline uint32_t instr_fetch(vaddr_t *pc, int len) {
+	printf("Debug info: pc is 0x%x\n", *pc);
   uint32_t instr = vaddr_ifetch(*pc, len);
   (*pc) += len;
   return instr;

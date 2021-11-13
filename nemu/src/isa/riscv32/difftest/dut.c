7cfcab2aed5e85c4d69a062994bf9e54fed7eeec
tracer-ics2021 <tracer@njuics.org>
2021-11-13 10:49:30 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: c2c140b081e24d5abe5f5d2be71788e6   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  10:49:30 up 12 min,  2 users,  load average: 0.18, 0.26, 0.21

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -3,6 +3,10 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+	if (ref_r->pc != cpu.pc) {
+		log_write("Difftest info: execute faild with instruction at 0x%x\n", pc);
+		return false;
+	}
 	for (int i = 0; i < 32; i++) {
 		if (ref_r->gpr[i]._32 != cpu.gpr[i]._32) {
 			log_write("Difftest info: execute faild with instruction at 0x%x\n", pc);

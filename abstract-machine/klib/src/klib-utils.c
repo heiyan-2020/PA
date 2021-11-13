965dc1e938ed6ce55f857de1b15e2c113287f4a9
tracer-ics2021 <tracer@njuics.org>
2021-11-13 21:29:33 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 6800c070e6414211929c4a518a7d8e8c   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  21:29:33 up  2:02,  3 users,  load average: 0.09, 0.14, 0.14

--- abstract-machine/klib/src/klib-utils.c
+++ abstract-machine/klib/src/klib-utils.c
@@ -35,7 +35,7 @@ char* print_template(char* out, const char* fmt, va_list ap) {
 						}
 						case 's': {
 							s = va_arg(ap, char*);
-							printf("0x%d, 0x%d\n", out, s);
+							printf("0x%x, 0x%x\n", out, s);
 							memcpy(out, s, strlen(s));
 							out += strlen(s);
 							break;					

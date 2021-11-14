c3e811cf8be8b514f6afedb70a141b3c96ffdff6
tracer-ics2021 <tracer@njuics.org>
2021-11-14 20:21:16 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 6d9bfe13c87444ad8e5e0367587bae2f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  20:21:16 up  2:41,  5 users,  load average: 0.77, 0.85, 0.86

--- nemu/src/isa/riscv32/instr/sysinst.h
+++ nemu/src/isa/riscv32/instr/sysinst.h
@@ -3,10 +3,16 @@ def_EHelper(ecall) {
 	rtl_li(s, &(s->dnpc), isa_raise_intr(1, cpu.mtvec));
 }
 #define MTVEC 0x00000305
+#define MCAUSE 0x00000342
+#define MSTATUS 0x00000300
+#define MEPC	0x00000341
 def_EHelper(csrrw) {
 	rtlreg_t* system_reg;
 	switch (id_src2->imm) {
 		case MTVEC: system_reg = &cpu.mtvec;break;
+		case MCAUSE: system_reg = &cpu.mcause;break;
+		case MSTATUS: system_reg = &cpu.mstatus;break;
+		case MEPC: system_reg = &cpu.mepc;break;
 		default: assert(0);
 	}
 	if (ddest != &cpu.gpr[0]._32) {
@@ -19,6 +25,9 @@ def_EHelper(csrrs) {
 	rtlreg_t* system_reg;
 	switch (id_src2->imm) {
 		case MTVEC: system_reg = &cpu.mtvec;break;
+		case MCAUSE: system_reg = &cpu.mcause;break;
+		case MSTATUS: system_reg = &cpu.mstatus;break;
+		case MEPC: system_reg = &cpu.mepc;break;
 		default: assert(0);
 	}
 	rtl_addi(s, ddest, system_reg, 0);

893484cee8d77452b01ade6de3f0a6e4683dd3c6
tracer-ics2021 <tracer@njuics.org>
2021-11-14 20:46:35 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 6d9bfe13c87444ad8e5e0367587bae2f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  20:46:35 up  3:06,  5 users,  load average: 0.62, 0.71, 0.73

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -4,12 +4,12 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 	if (ref_r->pc != cpu.pc) {
-		log_write("Difftest info: execute faild with instruction at 0x%x\n", pc);
+		log_write("Difftest info: execute faild with instruction at 0x%x, caused by pc not equal. expected: 0x%x, but got: 0x%x\n", pc, ref_r->pc, cpu.pc);
 		return false;
 	}
 	for (int i = 0; i < 32; i++) {
 		if (ref_r->gpr[i]._32 != cpu.gpr[i]._32) {
-			log_write("Difftest info: execute faild with instruction at 0x%x\n", pc);
+			log_write("Difftest info: execute faild with instruction at 0x%x, caused by the %dth register not equal. expected: 0x%x, but got: 0x%x\n", pc, i, ref_r->gpr[i]._32, cpu.gpr[i]._32);
 			return false;
 		}
 	}

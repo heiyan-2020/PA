7f6c6b1436e9de48afa16668a622c9126537a8c0
tracer-ics2021 <tracer@njuics.org>
2021-11-14 23:08:21 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 6d9bfe13c87444ad8e5e0367587bae2f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  23:08:21 up  5:28,  5 users,  load average: 0.67, 0.87, 0.81

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -5,6 +5,9 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    * Then return the address of the interrupt/exception vector.
    */
 	//at present, pc is ecall itself. need to determine whether +4 according to the exception type.
+#ifdef CONFIG_ETRACE
+	log_write("[ETRACE INFO] program interrupted at 0x%x, caused NO.%d exception type.\n", epc, NO);
+#endif
 	cpu.mepc = cpu.pc;
 	cpu.mcause = NO;
   return epc;

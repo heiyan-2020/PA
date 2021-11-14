5ba05509c4e1a381c9a1b38b9092f798805ede8f
tracer-ics2021 <tracer@njuics.org>
2021-11-14 22:50:16 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 6d9bfe13c87444ad8e5e0367587bae2f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  22:50:16 up  5:10,  5 users,  load average: 0.51, 0.54, 0.72

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-		case EVENT_YIELD: printf("yield succuss!\n");break;
+		case EVENT_YIELD: printf("yield succuss!\n"); c->mepc += 4;break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -4,8 +4,8 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-	cpu.mepc = cpu.pc + 4;
-	printf("mepc is %x\n", cpu.mepc);
+	//at present, pc is ecall itself. need to determine whether +4 according to the exception type.
+	cpu.mepc = cpu.pc;
 	cpu.mcause = NO;
   return epc;
 }

bf0b62cced1446a2b19aa8794773756defa7ba78
tracer-ics2021 <tracer@njuics.org>
2021-11-14 20:56:08 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 6d9bfe13c87444ad8e5e0367587bae2f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  20:56:08 up  3:16,  5 users,  load average: 0.83, 0.80, 0.74

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,6 +7,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
+		printf("EVENT_YIELD is %d\n", EVENT_YIELD);
     switch (c->mcause) {
 			case 1: ev.event = EVENT_YIELD;	break;
       default: ev.event = EVENT_ERROR; break;

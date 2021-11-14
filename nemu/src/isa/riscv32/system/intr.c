2e9033a572d8a2aba11c4cc3530d1f2a01af5f75
tracer-ics2021 <tracer@njuics.org>
2021-11-14 22:43:01 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 6d9bfe13c87444ad8e5e0367587bae2f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  22:43:01 up  5:03,  5 users,  load average: 0.52, 0.74, 0.87

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -4,7 +4,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-	cpu.mepc = cpu.pc;
+	cpu.mepc = cpu.pc + 4;
 	printf("mepc is %x\n", cpu.mepc);
 	cpu.mcause = NO;
   return epc;

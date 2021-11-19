54a7c9d9460887af46f96705e6cba80ab2375ba0
tracer-ics2021 <tracer@njuics.org>
2021-11-19 15:17:16 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 12d57af441d6497f9bca75b872231f33   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  15:17:16 up  2:33,  5 users,  load average: 0.17, 0.21, 0.21

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -14,6 +14,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 	//read the elf_header.
 	ramdisk_read(elf_header, 0, sizeof(Elf_Ehdr));
 	//verify file type.
+	printf("0x%x\n", *(uint32_t *)elf_header->e_ident);	
 	assert(*(uint32_t *)elf_header->e_ident == 0x7f454C46);
 	//read the program_header.
 	Elf_Phdr prog_header[1];

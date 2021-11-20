d963751ede812bfe6be418f06237103ce3132bcc
tracer-ics2021 <tracer@njuics.org>
2021-11-20 11:12:55 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: aa3a6f441be9449f98eb5a7763108406   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  11:12:55 up  1:23,  4 users,  load average: 0.08, 0.21, 0.18

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,17 +1,19 @@
 #include <common.h>
 #include "syscall.h"
 
-int sys_yield() {
+void sys_yield(Context *c) {
 	yield();
-	return 0;
+	c->GPRx = 0;
 }
+
+void sys_exit();
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
-		case SYS_yield: sys_yield();break;
+		case SYS_yield: sys_yield(c);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }

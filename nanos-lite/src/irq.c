52cf42940f3dab5b3b3c6da8ff5c62ae817e16f5
tracer-ics2021 <tracer@njuics.org>
2021-11-20 11:06:33 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: aa3a6f441be9449f98eb5a7763108406   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  11:06:33 up  1:17,  4 users,  load average: 0.29, 0.23, 0.19

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@ void do_syscall(Context*);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
 		case EVENT_YIELD: printf("yield succuss!\n"); c->mepc += 4;break;
-		case EVENT_SYSCALL: do_syscall(c);
+		case EVENT_SYSCALL: do_syscall(c);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 

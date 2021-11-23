b18011600b49deef38b6470c61c5d283375b83a2
tracer-ics2021 <tracer@njuics.org>
2021-11-23 11:25:54 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 29bddcd1e9fc4279b3ab1f87e9734c7c   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  11:25:54 up  1:25,  1 user,  load average: 0.19, 0.22, 0.19

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -14,10 +14,12 @@ void sys_exit(Context* c) {
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+#ifdef STRACE
+	printf("[STRACE]\tcalled %s, with arguments\t0x%x\t0x%x\t0x%x,returned 0x%x\n", CALL_STRING[a[0]], c->GPR2, c->GPR3, c->GPR4, c->GPRx);
+#endif
   switch (a[0]) {
-		case SYS_exit:log_write("[STRACE]\tcalled[SYS_exit], \treturned 0x%x\n", c->GPRx);sys_exit(c);break;
-		case SYS_yield: sys_yield(c);log_write("[STRACE]\tcalled[SYS_yield], \treturned 0x%x\n", c->GPRx);break;
+		case SYS_exit:sys_exit(c);break;
+		case SYS_yield: sys_yield(c);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }

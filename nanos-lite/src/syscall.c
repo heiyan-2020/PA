7dab9be0ceb4c7db64ae0dda97c0024b5215ac7a
tracer-ics2021 <tracer@njuics.org>
2021-11-23 15:18:08 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: bb3d2cad70ba4c1199a1ca9d3b92676b   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  15:18:08 up 26 min,  3 users,  load average: 0.30, 0.23, 0.15

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,18 +8,35 @@ void sys_yield(Context *c) {
 
 void sys_exit(Context* c) {
 	halt(c->GPR2);
-	c->GPRx = 0;
+	c->GPRx = c->GPR2 & 0xFF;
+}
+
+void sys_write(Context* c) {
+	int fd = c->GPR2;
+	const char* buf = (char*)c->GPR3;
+	size_t count = c->GPR4;
+	if (fd == 1 || fd == 2) {
+		for (int i = 0; i < count; i++) {
+			putch(buf[i]);
+		}	
+	}
+	c->GPRx = count;
+	//Attention!I have not handled situation that the number of wtitten bytes less than count.
 }
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 #ifdef STRACE
+	if (a[0] < 0 || a[0] > 19) {
+		panic("Illegal Syscall ID = %d\n", a[0]);
+	}
 	printf("[STRACE]\tcalled %s, with arguments\t0x%x\t0x%x\t0x%x,returned 0x%x\n", CALL_STRING[a[0]], c->GPR2, c->GPR3, c->GPR4, c->GPRx);
 #endif
   switch (a[0]) {
 		case SYS_exit:sys_exit(c);break;
 		case SYS_yield: sys_yield(c);break;
+		case SYS_write: sys_write(c);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }

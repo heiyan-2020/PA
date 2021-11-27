aa35c6ce916c625e2c11ea7ad7987531f4ecb0f9
tracer-ics2021 <tracer@njuics.org>
2021-11-27 17:07:30 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 0e9c6aa252a94031b3983b3e0c5cce7e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  17:07:30 up 13 min,  4 users,  load average: 0.25, 0.28, 0.19

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -7,13 +7,13 @@ int main() {
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
-	printf("size is %d\n", size);
   assert(size == 5000);
 
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
+		printf("n = %d, i = %d\n", n, i);
     assert(n == i + 1);
   }
 

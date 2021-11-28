fa99ea519350a089c98be664c92e8ee9acd4bcff
tracer-ics2021 <tracer@njuics.org>
2021-11-28 14:55:21 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 7ce725f7ac4d45ea876b89be5504b86a   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  14:55:22 up 56 min,  4 users,  load average: 0.28, 0.20, 0.22

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -54,7 +54,7 @@ void NDL_OpenCanvas(int *w, int *h) {
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
 		int real_x = canvas_x + x;
 	 	int real_y = canvas_y + y;
-		int fd = _open("dev/fb", 0, 0);
+		int fd = _open("/dev/fb", 0, 0);
 		_lseek(fd, (real_y * screen_w + real_x) * 4, SEEK_SET);
 		for (int i = 0; i < h; i++) {
 			_write(fd, pixels, w*4);

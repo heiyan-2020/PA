5a6a1d6e491e00c5cbc81de25a370b5cfc337ae1
tracer-ics2021 <tracer@njuics.org>
2021-11-28 19:11:11 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 7ce725f7ac4d45ea876b89be5504b86a   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  19:11:12 up  5:12,  4 users,  load average: 0.24, 0.41, 0.44

--- navy-apps/tests/fixedpt-test/main.c
+++ navy-apps/tests/fixedpt-test/main.c
@@ -8,5 +8,9 @@ int main() {
 	if (b > fixedpt_rconst(7.9)) {
   	c = fixedpt_toint(fixedpt_div(fixedpt_mul(a + FIXEDPT_ONE, b), fixedpt_rconst(2.3)));
 	}
+	printf("c = %d\n", c);
 	assert(c == 9);
+	fixedpt d = fixedpt_rconst(-1.7);
+	c = fixedpt_floor(d) + fixedpt_ceil(a);
+	assert(fixedpt_rconst(c) == 0);
 }

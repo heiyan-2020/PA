97e056759a6dc8ecb7de8f9cfb55a07176cd99d7
tracer-ics2021 <tracer@njuics.org>
2021-11-29 19:27:28 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 332a351ec4cc4c35800de6447b3dcb58   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  19:27:28 up  3:21,  4 users,  load average: 0.71, 0.65, 0.61

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -70,6 +70,7 @@ static void clear_display(void) {
 int main(int argc, char *argv[], char *envp[]) {
   SDL_Init(0);
   screen = SDL_SetVideoMode(0, 0, 32, SDL_HWSURFACE);
+	printf("screen: x = %d, y = %d, w = %d, h = %d\n", screen->x, screen->y, screen->w, screen->h);
 
   font = new BDF_Font(font_fname);
   logo_sf = SDL_LoadBMP("/share/pictures/projectn.bmp");
@@ -142,7 +143,6 @@ static void draw_text_row(char *s, int r) {
 
 static void display_menu(int n) {
   clear_display();
-	printf("after clear_display font's w = %d\n", font->w);
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
 	printf("after SDL_BlitSurface font's w = %d\n", font->w);

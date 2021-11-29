8d0cdb56eeadd474cdb9983e2d1dc3e8e32eee28
tracer-ics2021 <tracer@njuics.org>
2021-11-29 13:33:58 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: dd6f00f82c474af8b3baef0222707e04   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  13:33:58 up  1:25,  4 users,  load average: 0.43, 0.37, 0.35

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -31,6 +31,7 @@ void render() {
 }
 
 void prev(int rep) {
+				printf("prev\n");
   if (rep == 0) rep = 1;
   cur -= rep;
   if (cur < 0) cur = 0;
@@ -38,6 +39,7 @@ void prev(int rep) {
 }
 
 void next(int rep) {
+				printf("next\n");
   if (rep == 0) rep = 1;
   cur += rep;
   if (cur >= N) cur = N - 1;
@@ -56,7 +58,6 @@ int main() {
     SDL_WaitEvent(&e);
 
     if (e.type == SDL_KEYDOWN) {
-			printf("sym=%d\n", e.key.keysym.sym);
       switch(e.key.keysym.sym) {
         case SDLK_0: rep = rep * 10 + 0; break;
         case SDLK_1: rep = rep * 10 + 1; break;

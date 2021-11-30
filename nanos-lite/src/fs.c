d139cd8f8187405c01a6930b5af3c0226becd060
tracer-ics2021 <tracer@njuics.org>
2021-11-30 10:03:02 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 160f19d8ff8f4f6fabe9a1cb69515830   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  10:03:02 up  1:09,  4 users,  load average: 0.33, 0.41, 0.32

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -111,7 +111,7 @@ size_t fs_lseek(int fd, size_t offset, int whence) {
 	Finfo* currentFile = &file_table[fd];
 	switch (whence) {
 		case SEEK_SET: {
-							   printf("offset = %d\n", offset);
+							   printf("offset = %d, size = %d\n", offset, currentFile->size);
 											assert(offset <= currentFile->size);
 											currentFile->open_offset = offset;
 											return offset;	

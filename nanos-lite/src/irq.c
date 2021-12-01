500dfbcf2ea221a1eaa612d27a3802082915d176
tracer-ics2021 <tracer@njuics.org>
2021-12-01 17:51:44 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 4ff76232b7b943af94100911a8979606   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  17:51:44 up  8:27,  4 users,  load average: 0.13, 0.28, 0.39

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,7 +1,9 @@
 #include <common.h>
 void do_syscall(Context*);
 extern uint32_t _stack_top;
+extern uint32_t _stack_pointer;
 static Context* do_event(Event e, Context* c) {
+		printf("stack_pointer = 0x%x\n", _stack_pointer);
 		printf("0x%x\n", &_stack_top);
   switch (e.event) {
 		case EVENT_YIELD: 

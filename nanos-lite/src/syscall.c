73e845c081253d666bc0a271c6c68a3c6ab817d5
tracer-ics2021 <tracer@njuics.org>
2021-12-01 20:02:09 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 4ff76232b7b943af94100911a8979606   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  20:02:09 up 10:37,  4 users,  load average: 0.57, 0.56, 0.60

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -71,7 +71,7 @@ void sys_gettimeofday(Context* c) {
 			c->GPRx = -1;
 	} else {
 		time_t usec = io_read(AM_TIMER_UPTIME).us;
-		printf("us = %x\n", usec);
+		printf("us = %ld\n", usec);
 		tv->tv_sec = usec / 1000000;
 		tv->tv_usec = usec - tv->tv_sec * 1000000;
 		printf("%d\n", tv->tv_usec);

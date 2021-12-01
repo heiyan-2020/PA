9b35fb0b5d08c7702e30e88ad704de984582256e
tracer-ics2021 <tracer@njuics.org>
2021-12-01 19:09:31 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 4ff76232b7b943af94100911a8979606   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  19:09:31 up  9:44,  4 users,  load average: 0.44, 0.76, 1.41

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -60,13 +60,14 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 		}
 }
 
+
 size_t fb_write(const void *buf, size_t offset, size_t len) {
 	int screen_w = io_read(AM_GPU_CONFIG).width;
 	int current_h = (offset / 4) / screen_w;
 	int current_w = (offset / 4) % (screen_w);
-	printf("current_h = %d, current_w = %d\n", current_h, current_w);
 	int remain = len - ((screen_w) - (current_w)) * 4;
 	if (remain <  0) {
+			//write all len bytes;
 		io_write(AM_GPU_FBDRAW, current_w, current_h, (void*)buf, (len / 4), 1, true);
 		return len;
 	} else {

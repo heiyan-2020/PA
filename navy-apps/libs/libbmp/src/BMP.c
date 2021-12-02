453c3035771186b9c89f0988226b365ed4380b86
tracer-ics2021 <tracer@njuics.org>
2021-12-02 12:35:48 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: eda423d19d61424c9069297bf42e2e58   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  12:35:48 up  3:39,  4 users,  load average: 0.22, 0.28, 0.25

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -22,12 +22,15 @@ struct BitmapHeader {
 void* BMP_Load(const char *filename, int *width, int *height) {
   FILE *fp = fopen(filename, "r");
   if (!fp) return NULL;
+	printf("Don't return from NULL\n");
   struct BitmapHeader hdr;
   assert(sizeof(hdr) == 54);
   assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
 
   if (hdr.bitcount != 24) return NULL;
+	printf("Don't return from bitcount\n");
   if (hdr.compression != 0) return NULL;
+	printf("Don't return from compression\n");
   int w = hdr.width;
   int h = hdr.height;
   uint32_t *pixels = malloc(w * h * sizeof(uint32_t));
@@ -47,7 +50,5 @@ void* BMP_Load(const char *filename, int *width, int *height) {
   fclose(fp);
   if (width) *width = w;
   if (height) *height = h;
-	assert(pixels);
-	printf("assert success\n");
   return pixels;
 }

fdb5991abeb4ee260248e07594286468d494188b
tracer-ics2021 <tracer@njuics.org>
2021-12-03 00:14:10 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: c62b6c63ccd14db09f3c6844253fd87d   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  00:14:10 up  2:32,  3 users,  load average: 0.20, 0.17, 0.54

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -69,9 +69,9 @@ void *_sbrk(intptr_t increment) {
 	if (_syscall_(SYS_brk, increment, 0, 0) == 0) {
 		preBreak = program_break;
 		program_break += increment;
-		char buf[64];
-		fprintf(buf, "increment = %d, sbrk = 0x%x\n", increment, program_break);
-		_write(1, buf, 64);
+//		char buf[64];
+//		fprintf(buf, "increment = %d, sbrk = 0x%x\n", increment, program_break);
+//		_write(1, buf, 64);
 		return (void*)preBreak;
 	}
 	return (void*)-1;

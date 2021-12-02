6d2b44ae33f600c2f2f1d95eb4ec11feb172d7fe
tracer-ics2021 <tracer@njuics.org>
2021-12-02 09:57:07 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: eda423d19d61424c9069297bf42e2e58   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  09:57:07 up  1:00,  3 users,  load average: 0.42, 0.22, 0.21

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -48,7 +48,7 @@ intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
   register intptr_t ret asm (GPRx);
   asm volatile (SYSCALL : "=r" (ret) : "r"(_gpr1), "r"(_gpr2), "r"(_gpr3), "r"(_gpr4));
 	if (type == 19) {
-		printf("p = %ld\n, a0 = 0x%x\n", ((struct timeval*) a0)->tv_usec, a0);
+		printf("p = %ld, a0 = 0x%x\n", ((struct timeval*) a0)->tv_usec, &(((struct timeval*) a0)->tv_usec));
 	}
   return ret;
 }

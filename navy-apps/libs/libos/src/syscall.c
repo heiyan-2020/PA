82e432c029c69306d7ebd44c793c4cf155bb721c
tracer-ics2021 <tracer@njuics.org>
2021-12-02 10:13:05 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: eda423d19d61424c9069297bf42e2e58   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  10:13:05 up  1:16,  3 users,  load average: 0.35, 0.24, 0.27

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -49,6 +49,7 @@ intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
   asm volatile (SYSCALL : "=r" (ret) : "r"(_gpr1), "r"(_gpr2), "r"(_gpr3), "r"(_gpr4));
 	if (type == 19) {
 		printf("p = %ld, a0 = 0x%x\n", ((struct timeval*) a0)->tv_usec, &(((struct timeval*) a0)->tv_usec));
+		printf("%d\n", sizeof(long));
 	}
   return ret;
 }

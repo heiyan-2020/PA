84635ad604c72d6bbc7ea762c88b3f6282cb00f7
tracer-ics2021 <tracer@njuics.org>
2021-12-04 10:49:40 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: d9d30be34fc04159976677ed6504b776   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  10:49:40 up 23 min,  4 users,  load average: 0.22, 0.17, 0.13

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -22,8 +22,48 @@ static void sh_prompt() {
   sh_printf("sh> ");
 }
 
-static void sh_handle_cmd(const char *cmd) {
-				printf("%s\n",cmd);
+static int cmd_echo(char* str) {
+	sh_printf("%s\n", str);
+	return 0;
+}
+
+static int cmd_run(char* path) {
+	execve(path, NULL, NULL);
+	return 0;
+}
+
+static struct {
+	const char* name;
+	const char* description;
+	int (*handler) (char*);
+} cmd_table[] = {
+	"echo", "display a line of text", cmd_echo,
+	"/", "run a procedure", cmd_run
+};
+static const int NR_CMD = sizeof(cmd_table) / sizeof(cmd_table[0]);
+static void sh_handle_cmd(char *_cmd) {
+	char *cmd = strtok(_cmd, " ");
+	if (cmd != NULL) {
+		char *args = cmd + strlen(_cmd) + 1;
+		char* str_end = _cmd + strlen(_cmd);
+		if (args >= str_end) {
+			args = NULL;
+		}
+		int i;
+		for (i = 0; i < NR_CMD; i++) {
+			if (strcmp(cmd, cmd_table[i].name) == 0) {
+				if (i == 1) {
+					cmd_run(cmd);
+				} else {
+					cmd_table[i].handler(args);
+				}
+				break;
+			}
+		}
+		if (i == NR_CMD) {
+			return;
+		}
+	}
 }
 
 void builtin_sh_run() {
@@ -36,7 +76,10 @@ void builtin_sh_run() {
       if (ev.type == SDL_KEYUP || ev.type == SDL_KEYDOWN) {
         const char *res = term->keypress(handle_key(&ev));
         if (res) {
-          sh_handle_cmd(res);
+					assert(strlen(res) < 128);
+					char buf[128];
+					strcpy(buf, res);
+          sh_handle_cmd(buf);
           sh_prompt();
         }
       }

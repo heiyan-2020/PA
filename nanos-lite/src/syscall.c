e7454f6281932f3d87e03717f8c831391a3825b3
tracer-ics2021 <tracer@njuics.org>
2021-12-09 17:11:21 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 1cf9c3391cbd4e138a4abbd647145cae   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  17:11:21 up  1:06,  4 users,  load average: 0.54, 0.47, 0.34

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,6 +8,7 @@ void context_uload(PCB* proc, const char* pathname, char* const argv[], char* co
 void switch_boot_pcb();
 
 int sys_execve(const char* pathname, char* const argv[], char* const envp[]) {
+				printf("%s\n", argv[1]);
 	context_uload(current, pathname, argv, envp);
 	switch_boot_pcb();
 	yield();	

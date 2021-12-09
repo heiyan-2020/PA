60bcb1a665a31cd963a40fdc330b53c9788890c9
tracer-ics2021 <tracer@njuics.org>
2021-12-09 10:31:55 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 2ae8b67dc18440ba874c4f54a9f37c61   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  10:31:56 up 20 min,  4 users,  load average: 0.28, 0.31, 0.28

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -31,10 +31,8 @@ static int cmd_run(char* path, char* args) {
 	char* pt = path + strlen(path);
 	while (*pt != '\n') pt--;
 	*pt = '\0';
+	args = strtok(NULL, " ");
 	char** argv = (char**) malloc(16 * sizeof(char*));
-	while (1) {
-		printf("here\n");
-	}
 	for (int i = 0; i < 16; i++) {
 		argv[i] = strtok(NULL, " ");
 		if (argv[i] == NULL) {

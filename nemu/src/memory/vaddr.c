bd8a6a07d7dec098af62f37c383e0901e5d7733c
tracer-ics2021 <tracer@njuics.org>
2021-12-11 16:53:07 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: b763536c32714142b00be90fc762746f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  16:53:07 up  1:40,  3 users,  load average: 0.20, 0.44, 0.46

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -4,7 +4,9 @@
 word_t vaddr_ifetch(vaddr_t addr, int len) {
 	if (isa_mmu_check(addr, len, MEM_TYPE_IFETCH) == MMU_TRANSLATE) {
 		word_t ret = isa_mmu_translate(addr, len, MEM_TYPE_IFETCH);
-		addr = ret;
+		if (addr == ret) {
+			addr = ret;
+		}
 	}
   return paddr_read(addr, len);
 }
@@ -13,7 +15,9 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
 word_t vaddr_read(vaddr_t addr, int len) {
 	if (isa_mmu_check(addr, len, MEM_TYPE_READ) == MMU_TRANSLATE) {
 		word_t ret =  isa_mmu_translate(addr, len, MEM_TYPE_READ);
-		addr = ret;
+		if (addr == ret) {
+			addr = ret;
+		}
 	}
   return paddr_read(addr, len);
 }
@@ -21,7 +25,9 @@ word_t vaddr_read(vaddr_t addr, int len) {
 void vaddr_write(vaddr_t addr, int len, word_t data) {
 if (isa_mmu_check(addr, len, MEM_TYPE_WRITE) == MMU_TRANSLATE) {
 		word_t ret =  isa_mmu_translate(addr, len, MEM_TYPE_WRITE);
-		addr = ret;
+		if (addr == ret) {
+			addr = ret;
+		}
 	}
   paddr_write(addr, len, data);
 }

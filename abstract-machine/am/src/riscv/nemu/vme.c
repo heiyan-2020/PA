9dccc10f9c8ae7b41c52e25ce5fcfde192e17c19
tracer-ics2021 <tracer@njuics.org>
2021-12-11 15:45:42 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: b763536c32714142b00be90fc762746f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  15:45:42 up 33 min,  3 users,  load average: 0.61, 0.46, 0.39

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -14,8 +14,10 @@ static Area segments[] = {      // Kernel memory mappings
 #define USER_SPACE RANGE(0x40000000, 0x80000000)
 
 static inline void set_satp(void *pdir) {
+				printf("b\n");
   uintptr_t mode = 1ul << (__riscv_xlen - 1);
   asm volatile("csrw satp, %0" : : "r"(mode | ((uintptr_t)pdir >> 12)));
+	printf("a\n");
 }
 
 static inline uintptr_t get_satp() {
@@ -30,14 +32,12 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
 
   kas.ptr = pgalloc_f(PGSIZE);
   int i;
-	printf("b\n");
   for (i = 0; i < LENGTH(segments); i ++) {
     void *va = segments[i].start;
     for (; va < segments[i].end; va += PGSIZE) {
       map(&kas, va, va, 0);
     }
   }
-	printf("a\n");
   set_satp(kas.ptr);
   vme_enable = 1;
 

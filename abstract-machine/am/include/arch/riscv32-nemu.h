4ab687a671663ce13f183aab4489d9aa19d9fd68
tracer-ics2021 <tracer@njuics.org>
2021-12-11 19:35:46 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: f30b58a302964f09af9c86662c2d4a2f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  19:35:46 up 54 min,  3 users,  load average: 0.79, 0.41, 0.35

--- abstract-machine/am/include/arch/riscv32-nemu.h
+++ abstract-machine/am/include/arch/riscv32-nemu.h
@@ -3,9 +3,13 @@
 
 struct Context {
   // TODO: fix the order of these members to match trap.S
-	uintptr_t gpr[32], mcause, mstatus, mepc;
+	union {
+		void* pdir;
+		uintptr_t gpr[32];	
+	};
+	uintptr_t mcause, mstatus, mepc;
 	//may need to change place.
-  void *pdir;
+//  void *pdir;
 };
 
 #define GPR1 gpr[17] // a7

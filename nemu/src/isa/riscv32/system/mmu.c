28fd77a68ea1da75ec531bfa22364b628f8477fb
tracer-ics2021 <tracer@njuics.org>
2021-12-11 15:16:34 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: b763536c32714142b00be90fc762746f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  15:16:34 up 4 min,  3 users,  load average: 0.50, 0.75, 0.37

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -3,7 +3,7 @@
 #include <memory/vaddr.h>
 
 int isa_mmu_check(vaddr_t vaddr, int len, int type) {
-	uint32_t tmp_satp = (uint32_t)cpu.satp;
+	word_t tmp_satp = (word_t)cpu.satp;
 	if (tmp_satp >> 31) {
 		return MMU_TRANSLATE;
 	} else {
@@ -14,18 +14,17 @@ int isa_mmu_check(vaddr_t vaddr, int len, int type) {
 #define PGTABLE_WIDTH 10
 #define	VALID_MASK 1
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-	uintptr_t va = (uintptr_t)cpu.satp;
-	printf("va = 0x%lx\n", va);
-	printf("size = %ld\n", sizeof(va));
-	uintptr_t pg_dic_num = va >> (PGSIZE_WIDTH + PGTABLE_WIDTH);
-	uintptr_t pg_table_num = (va >> (PGSIZE_WIDTH)) & ((1 << PGTABLE_WIDTH) - 1);
-	uintptr_t vaddr_offset = vaddr & ((1 << PGSIZE_WIDTH) - 1);
-	uintptr_t* pg_dic = (uintptr_t*)(va << (PGTABLE_WIDTH));	
-	printf("pg_dic = 0x%ln, pg_dic_num = %ld\n", pg_dic, pg_dic_num);
+	word_t va = (word_t)cpu.satp;
+	printf("va = 0x%x\n", va);
+	word_t pg_dic_num = va >> (PGSIZE_WIDTH + PGTABLE_WIDTH);
+	word_t pg_table_num = (va >> (PGSIZE_WIDTH)) & ((1 << PGTABLE_WIDTH) - 1);
+	word_t vaddr_offset = vaddr & ((1 << PGSIZE_WIDTH) - 1);
+	word_t* pg_dic = (word_t*)(va << (PGTABLE_WIDTH));	
+	printf("pg_dic = 0x%ls, pg_dic_num = %d\n", pg_dic, pg_dic_num);
 	if ((pg_dic[pg_dic_num] & VALID_MASK) == 0) {
 			return MEM_RET_FAIL;
 	}
-	uintptr_t* pg_table = (uintptr_t*)(pg_dic[pg_dic_num] >> (PGSIZE_WIDTH)); 
+	word_t* pg_table = (word_t*)(pg_dic[pg_dic_num] >> (PGSIZE_WIDTH)); 
 	if ((pg_table[pg_table_num] & VALID_MASK) == 0) {
 		return MEM_RET_FAIL;
 	}

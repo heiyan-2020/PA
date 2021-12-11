ee78ce74e5f40c0a8fb1af9e04eaf83606e911fa
tracer-ics2021 <tracer@njuics.org>
2021-12-11 19:40:47 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: f30b58a302964f09af9c86662c2d4a2f   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  19:40:47 up 59 min,  3 users,  load average: 0.21, 0.25, 0.28

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -10,6 +10,7 @@ static Context* do_event(Event e, Context* c) {
 //					printf("yield succuss!\n"); 
 //					c->mepc += 4;
 					Context* ret = schedule(c);
+					assert(ret != NULL);
 					return ret;
 					break;
 				}

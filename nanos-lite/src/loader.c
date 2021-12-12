99826a407781a1c5830e2402d52e0cdf20b9791e
tracer-ics2021 <tracer@njuics.org>
2021-12-12 09:39:11 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 92ec9f9819c4484599cd858881434716   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  09:39:11 up 52 min,  4 users,  load average: 0.10, 0.26, 0.28

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -49,10 +49,8 @@ void load_helper(void* buf, int fd, int current) {
 		assert(rem < pgsize);
 		fs_read(fd, buf, rem);
 		memset(buf + rem, 0, pgsize - rem);
-		printf("current = 0x%x, filesz = 0x%x\n", current, prog_header->p_filesz);
 		uint32_t* pt = (uint32_t*) buf;
 		for (int i = 0; i < pgsize; i+=4) {
-			printf("0x%x: 0x%x\n",prog_header->p_vaddr + current + i,*pt);
 			pt++;
 		}
 	} else {

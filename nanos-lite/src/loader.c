757c1e200b7bca35ea399794c71d3dc7a722c206
tracer-ics2021 <tracer@njuics.org>
2021-12-12 09:55:57 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 92ec9f9819c4484599cd858881434716   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  09:55:57 up  1:09,  4 users,  load average: 0.32, 0.36, 0.35

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -59,6 +59,7 @@ void load_helper(void* buf, int fd, int current) {
 }
 
 void load_page(PCB* pcb, int fd) {
+				printf("newload\n");
 	pgsize = pcb->as.pgsize;
 	int page_num = prog_header->p_memsz / pcb->as.pgsize + 1;
 //	page_num = page_num > 0 ? page_num : 1;

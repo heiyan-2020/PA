6eba51b0008de293548f5415b88f02984e1a68f8
tracer-ics2021 <tracer@njuics.org>
2021-12-12 10:47:24 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 92ec9f9819c4484599cd858881434716   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  10:47:24 up  2:00,  4 users,  load average: 0.55, 0.57, 0.66

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -68,14 +68,14 @@ void load_page(PCB* pcb, int fd) {
 	AddrSpace* _as = &pcb->as;
 	fs_lseek(fd, prog_header->p_offset, SEEK_SET);
 	int current = 0;
-	int offset = ((uint32_t)vaddr) & (pgsize - 1);
-	if (offset != 0) {
-		void* page_frame = new_page(1);
-		map(_as, vaddr, page_frame, 1);
-		load_helper(page_frame + offset, fd, current, pgsize - offset);
-		vaddr += (pgsize - offset);
-		current += pgsize - offset;
-	}
+//	int offset = ((uint32_t)vaddr) & (pgsize - 1);
+//	if (offset != 0) {
+//		void* page_frame = new_page(1);
+//		map(_as, vaddr, page_frame, 1);
+//		load_helper(page_frame + offset, fd, current, pgsize - offset);
+//		vaddr += (pgsize - offset);
+//		current += pgsize - offset;
+//	}
 //	uint8_t buf[prog_header->p_memsz];
 //	fs_read(fd, buf, prog_header->p_filesz);
 //	memset(buf + prog_header->p_filesz, 0, prog_header->p_memsz - prog_header->p_filesz);

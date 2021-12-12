eb514bf4eef6891b7eb079717213a2f23d99ca34
tracer-ics2021 <tracer@njuics.org>
2021-12-12 13:27:57 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 75829ee1d9454b039a64c6365319338c   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  13:27:57 up  1:12,  4 users,  load average: 0.19, 0.32, 0.29

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -27,6 +27,7 @@ void free_page(void *p) {
 extern PCB* current;
 /* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
+				printf("brk = 0x%x, max_brk = 0x%x\n", brk, current->max_brk);
 	if (brk > current->max_brk) {
 		void* base = (void*)ROUNDUP(current->max_brk, PGSIZE);
 		int page_num = (brk - (uintptr_t)base) / PGSIZE + 1;

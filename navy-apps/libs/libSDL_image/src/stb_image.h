4b5e984f5717e6a7fe01d99e8eaaba37f3e6d869
tracer-ics2021 <tracer@njuics.org>
2022-01-10 16:02:26 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 9e061101f63b41e4adc4d9d91c908c97   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  16:02:26 up  1:04,  4 users,  load average: 0.45, 0.18, 0.12

--- navy-apps/libs/libSDL_image/src/stb_image.h
+++ navy-apps/libs/libSDL_image/src/stb_image.h
@@ -2706,7 +2706,7 @@ stbi_inline static int stbi__bitreverse16(int n)
       lut[i] = n;
     }
   }
-	printf("stb_image.h 2709:n = %d, addr of lut is %x\n",n, lut);
+	printf("stb_image.h 2709:n = %u, addr of lut is %x\n",(unsigned short)n, lut);
   return lut[(unsigned short)n];
 #else
   n = ((n & 0xAAAA) >>  1) | ((n & 0x5555) << 1);

7c9d5d30244ab21d78bb8e697650e73b060714a2
tracer-ics2021 <tracer@njuics.org>
2022-01-10 22:14:54 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: c704fb7036694c73ad03c25a5722c6c2   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  22:14:54 up  1:52,  3 users,  load average: 1.10, 0.36, 0.21

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -20,7 +20,7 @@ word_t vaddr_read(vaddr_t addr, int len) {
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
 	vaddr_t old = addr;
-if (isa_mmu_check(addr, len, MEM_TYPE_WRITE) == MMU_TRANSLATE) {
+	if (isa_mmu_check(addr, len, MEM_TYPE_WRITE) == MMU_TRANSLATE) {
 		addr =  isa_mmu_translate(addr, len, MEM_TYPE_WRITE);
 	}
 	if (addr == 0x82180406) {

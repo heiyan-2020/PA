770640cd60b184d6a0b32d32e2d84f558b0e9981
tracer-ics2021 <tracer@njuics.org>
2022-01-10 21:13:11 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: c704fb7036694c73ad03c25a5722c6c2   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  21:13:11 up 50 min,  3 users,  load average: 0.14, 0.17, 0.15

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -75,7 +75,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 		pg_dic[pg_dic_num] = (uint32_t)pgalloc_usr(PGSIZE);
 		pg_dic[pg_dic_num] |= 1;
 	}	
-	if (pg_dic_num == 0x101) {
+	if (pg_dic_num == 0x200) {
 			printf("pg_dic[num]=0x%x\n", pg_dic[pg_dic_num]);
 	}
 	uint32_t* pg_table = (uint32_t*)(((((uint32_t)pg_dic[pg_dic_num]) >> (PGSIZE_WIDTH))) << (PGSIZE_WIDTH)); 

d4756ab491bfeddabd7692c0819e2578108df0df
tracer-ics2021 <tracer@njuics.org>
2022-01-12 12:48:17 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 45757485cd9945f9a0c2932eff3b2def   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  12:48:17 up  1:49,  4 users,  load average: 0.53, 0.35, 0.20

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -14,23 +14,23 @@ int isa_mmu_check(vaddr_t vaddr, int len, int type) {
 #define PGTABLE_WIDTH 10
 #define	VALID_MASK 1
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-	printf("vaddr=0x%x\n", vaddr);
+//	printf("vaddr=0x%x\n", vaddr);
 	word_t va = (word_t)cpu.satp;
 	word_t pg_dic_num = vaddr >> (PGSIZE_WIDTH + PGTABLE_WIDTH);
 	word_t pg_table_num = (vaddr >> (PGSIZE_WIDTH)) & ((1 << PGTABLE_WIDTH) - 1);
 	word_t vaddr_offset = vaddr & ((1 << PGSIZE_WIDTH) - 1);
 	paddr_t pg_dic = (paddr_t)(va << (PGSIZE_WIDTH));	
 	word_t pg_dic_item = paddr_read(pg_dic + pg_dic_num * sizeof(word_t), sizeof(word_t));
-	printf("pg_dic = 0x%x, pg_dic_num = 0x%x, pg_dic_item = 0x%x\n",pg_dic, pg_dic_num, pg_dic_item);
+//	printf("pg_dic = 0x%x, pg_dic_num = 0x%x, pg_dic_item = 0x%x\n",pg_dic, pg_dic_num, pg_dic_item);
 	if ((pg_dic_item & VALID_MASK) == 0) {
 			return vaddr;
 	}
 	paddr_t pg_table = (paddr_t)(pg_dic_item >> (PGSIZE_WIDTH) << (PGSIZE_WIDTH)); 
 	word_t pg_table_item = paddr_read(pg_table + pg_table_num * sizeof(word_t), sizeof(word_t));
 	if ((pg_table_item & VALID_MASK) == 0) {
-		printf("not valid\n");
+//		printf("not valid\n");
 		return vaddr;
 	}
-	printf("mmu.c 32:pg_table=0x%x, pg_table_item=0x%x\n",pg_table ,pg_table_item);
+//	printf("mmu.c 32:pg_table=0x%x, pg_table_item=0x%x\n",pg_table ,pg_table_item);
 	return (pg_table_item & ~((1 << PGSIZE_WIDTH) - 1)) | vaddr_offset;
 }

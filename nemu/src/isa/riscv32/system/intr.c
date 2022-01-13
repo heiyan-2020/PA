f8594e15a967f62f7bcacb6eb581c904805813bb
tracer-ics2021 <tracer@njuics.org>
2022-01-13 22:44:05 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 2d0ed97089964963bc0f0b151ac3fb95   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  22:44:05 up 13:18,  4 users,  load average: 0.17, 0.10, 0.08

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -13,13 +13,13 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 	word_t mie = (cpu.mstatus >> 3) & 0x1;
 	cpu.mstatus = ((cpu.mstatus & ~(1 << 7)) | (mie << 7));
 	cpu.mstatus = ((cpu.mstatus & ~(1 << 3)) | (0 << 3));
+	printf("mstatus=0x%x\n", cpu.mstatus);
   return cpu.mtvec;
 }
 
 #define IRQ_TIMER 0x80000007
 word_t isa_query_intr() {
 	if (cpu.intr && ((cpu.mstatus >> 3) & 0x1)) {
-		printf("mstatus=0x%x\n", cpu.mstatus);
 		cpu.intr = false;
 		return IRQ_TIMER;
 	}

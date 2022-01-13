92bb10107404dd0039e75b4b92a3d924cfbd7c82
tracer-ics2021 <tracer@njuics.org>
2022-01-13 23:22:21 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: 2d0ed97089964963bc0f0b151ac3fb95   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  23:22:21 up 13:56,  4 users,  load average: 0.36, 0.37, 0.21

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -89,8 +89,8 @@ void init_proc() {
 	//context_kload(&pcb[0], hello_fun, "first!!!");
 	char* const argv[] = {"/bin/nterm", NULL};
 	context_uload(&pcb[0], "/bin/nterm", argv, NULL);
-//	char* const argv_1[] = {"/bin/hello", NULL};
-	context_uload(&pcb[1], "/bin/hello", NULL,NULL);
+	char* const argv_1[] = {"/bin/hello", NULL};
+	context_uload(&pcb[1], "/bin/hello", argv_1,NULL);
   switch_boot_pcb();
 
   Log("Initializing processes...");

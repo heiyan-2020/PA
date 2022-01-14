0dfb6902bf52a79ea3e2dc6bf0102f8f5bd320e6
tracer-ics2021 <tracer@njuics.org>
2022-01-14 09:23:42 +0800

    >  run 201250179    Static hostname: Legion          Icon name: computer-laptop            Chassis: laptop         Machine ID: 0c94845f56d949fcb5419aaed810c367            Boot ID: ec5359bfaa9a4c5baa444e52ad70283e   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-31-generic       Architecture: x86-64  09:23:42 up 45 min,  3 users,  load average: 0.26, 0.29, 0.34

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,7 +15,6 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-		printf("dia\n");
 		yield();
 	const char* charBuf = (const char*) buf;
 	size_t count = 0;
